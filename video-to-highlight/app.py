7985b28603ca4d58d673ef4fcda89d10ae9e701f
Jacky Wong <colabdog@gmail.com>
2023-10-14 16:00:01 -0700

    update reqs

---
+++ video-to-highlight/app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi import BaseModel
+
+app = FastAPI()
+
+class VideoHighlight(BaseModel):
+    videoToHighlight: str
+    objective: str
+
+@app.post("/videoHighlight")
+def highlight_video(video: VideoHighlight):
+    """
+    For a given video URL, returns a list of highlights
+    """
+    return {
+        "highlights": [
+            {
+                "order_number": 1,
+                "start_time": "00:00:10",
+                "end_time": "00:00:20",
+                "reason_for_highlight": "Interesting conversation"
+            }
+        ]
+    }
