ad91124a3b8146abf86f47c0da5b35cdd48c7226
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-14 16:55:57 -0700

    match features of a slide bar

--- extension/content.js
+++ extension/content.js
@@ -12,14 +12,18 @@ chrome.runtime.onMessage.addListener(
         const progressBarContainer = document.getElementsByClassName('ytp-progress-bar-container');
 
         const highlight = document.createElement('div');
-        highlight.style.position = 'absolute';
-        highlight.style.backgroundColor = 'green';
-        highlight.style.height = '5px';
-        highlight.style.width = '100%';
-        highlight.style.top = '0px';
-        highlight.style.left = '0px';
-        highlight.style.zIndex = '1000';
-        highlight.style.height = '100%';
+highlight.style.position = 'absolute';
+highlight.style.backgroundColor = 'green';
+highlight.style.height = '100%'; // Note: You've assigned this property twice
+highlight.style.width = '100%';
+highlight.style.top = '0px';
+highlight.style.left = '0px';
+highlight.style.zIndex = '1000';
+highlight.style.touchAction = 'none';
+highlight.role = 'slider';
+highlight.tabIndex = '0';  
+highlight.draggable = true; // Prefer boolean values when applicable
+
 
         console.log("appending highlight")
         if(progressBarContainer.length > 0) {
