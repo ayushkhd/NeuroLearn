7d0adc84e6195d3988c2f1a2c2d5ebb2eb152981
Akhil Dhavala <akhil.dhavala@gmail.com>
2023-10-14 18:46:24 -0700

    refactor

--- video-to-highlight/elevenlabs/client.py
+++ video-to-highlight/elevenlabs/client.py
@@ -2,41 +2,31 @@ from elevenlabs import set_api_key, Voice, VoiceSettings, generate, play, save, 
 import time
 
 
-
 api_key = '925011858db6abb00c9dacdbb13ef2c3' # Akhil Dhavala's API key
 voice_id = 'RE41IpVvESKH1SkTq04p' # Steve Jobs
 
 set_api_key(api_key)
 
-def stream_text_to_speech(text_to_speech: str) -> bytes:
-    audio_stream = generate(
-        text=text_to_speech,
-        voice=Voice(
-            voice_id=voice_id,
-            settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
-        )
-    )
-    return stream(audio_stream)
+class SteveJobsVoiceGenerator:
+    def __init__(self, voice_id):
+        self.voice_id = voice_id
 
-def play_text_to_speech(text_to_speech: str) -> bytes:
-    audio = generate(
-        text=text_to_speech,
-        voice=Voice(
-            voice_id=voice_id,
-            settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
-        )
-    )
-    return play(audio)
+    def generate_audio(self, text_to_speech):
+        voice_settings = VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
+        voice = Voice(voice_id=self.voice_id, settings=voice_settings)
+        audio = generate(text=text_to_speech, voice=voice)
+        return audio
 
-def save_text_to_speech(text_to_speech: str) -> None:
-    audio = generate(
-        text=text_to_speech,
-        voice=Voice(
-            voice_id=voice_id,
-            settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
-        )
-    )
-    timestamp = time.strftime("%Y%m%d%H%M%S")
-    filename = f"eleven_labs_steve_jobs_voice_{timestamp}.mp3"
-    save(audio, filename)
+    def stream_text_to_speech(self, text_to_speech):
+        audio = self.generate_audio(text_to_speech)
+        return stream(audio)
 
+    def play_text_to_speech(self, text_to_speech):
+        audio = self.generate_audio(text_to_speech)
+        return play(audio)
+
+    def save_text_to_speech(self, text_to_speech):
+        audio = self.generate_audio(text_to_speech)
+        timestamp = time.strftime("%Y%m%d%H%M%S")
+        filename = f"eleven_labs_steve_jobs_voice_{timestamp}.mp3"
+        save(audio, filename)
